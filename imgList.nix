f52ca52937e11f4bab2831c3e9c416857177d911
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-03-17 22:03:02 +0530

    add some elo

--- imgList.nix
+++ imgList.nix
@@ -10,4 +10,19 @@ helper: {
     url = "https://cdn.donmai.us/original/a6/7e/__sangonomiya_kokomi_genshin_impact_drawn_by_camunekoz__a67eed58a9e8796ee84eb7057704f01f.jpg";
     hash = "sha256-w1blRj2GXbl18eAgokb5o7NGbN7+mUSESOqGDud1ofc=";
   };
+  kokomi_eloone = helper {
+    name = "kokomi_eloone";
+    url = "https://cdn.donmai.us/original/c6/56/__sangonomiya_kokomi_genshin_impact_drawn_by_elodeas__c656613a8c3cf0142fd8404e2b3e9309.png";
+    hash = "sha256-lZoNJPNqrl3PxYDl+anP2vYhCXYbRGGJi7zZxMwb490=";
+  };
+  kokomi_elotwo = helper {
+    name = "kokomi_elotwo";
+    url = "https://cdn.donmai.us/original/05/ac/__sangonomiya_kokomi_genshin_impact_drawn_by_elodeas__05acd70d7ad25e8aeb8590eafeed0d73.png";
+    hash = "sha256-BxrlN7efs5Nf7l0PdopPMyon3gD5CYw+M4U/+W+2xqw=";
+  };
+  kokomi_elothree = helper {
+    name = "kokomi_elothree";
+    url = "https://cdn.donmai.us/original/ae/f3/__sangonomiya_kokomi_genshin_impact_drawn_by_elodeas__aef3b3fd04e76c3159c3f5c7a75e5883.jpg";
+    hash = "sha256-hcbq1OhX6cemStSxvoCtz7W+txoG5WKiH19BxudFoGY=";
+  };
 }
