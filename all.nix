bb53f4f094a4a041d9027120640018ee758ef046
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-03-20 15:22:08 +0530

    Hyper Optimizations

--- all.nix
+++ all.nix
@@ -3,7 +3,6 @@
   pkgs,
 }: let
   inherit (pkgs) lib system;
-
   parser = {id, ...}: let
     package = self.packages.${system}.${id};
   in {
@@ -12,36 +11,54 @@
     __metadata = package.metadata;
   };
   imgList = builtins.attrValues (import ./newImgList.nix parser);
+  imgListLen = lib.lists.length imgList;
 
-  getCategoryList = category: let
-    # this basically takes an image and returns a list of its tag_string_{$category} metadata
-    # i.e say you have image.metadata.tag_string_character = "lisa_(genshin_impact) barbara_(genshin_impact)"
-    # this function will convert it into ["lisa_(genshin_impact)" "barbara_(genshin_impact)"]
-    imgCategories = img: lib.splitString " " img.__metadata.${"tag_string_${category}"};
-  in
-    lib.pipe imgList [
-      (builtins.map imgCategories)
-      (lib.lists.flatten)
-      (lib.lists.unique)
-    ];
+  genList = {
+    charlist ? {},
+    artistlist ? {},
+    copyrightlist ? {},
+    index ? 0,
+  }: let
+    image = builtins.elemAt imgList index;
+    catList = category: lib.splitString " " image.__metadata.${"tag_string_${category}"};
+    imageCharList = catList "character";
+    imageArtistList = catList "artist";
+    imageCopyrightList = catList "copyright";
 
-  # to explain this fuckery to myself in the future first the final output of
-  # this is list of character folders with links to each img from the img list
-  # that contains any character from the characterList where characterList is
-  # is an overall flattened list of every character found in the imgList honest
-  # this would have been so much easier to understand if we had pipe operator
-  # out of experimental
-  # could make this a function but fuck it
-  getCategoryFolders = category:
-    builtins.map (element: rec {
-      name = element;
-      path = let
-        filter = img: builtins.elem element (lib.splitString " " img.__metadata.${"tag_string_${category}"});
+    mapElem = origlist: elem: {
+      ${elem} = let
+        oldList = lib.optionals (builtins.hasAttr elem origlist) origlist.${elem};
       in
-        pkgs.linkFarm name (builtins.filter filter imgList);
-    }) (getCategoryList category);
+        [image] ++ oldList;
+    };
+    getNewlist = origlist: list: lib.attrsets.mergeAttrsList (builtins.map (mapElem origlist) list);
+    charlist' = getNewlist charlist imageCharList;
+    artistlist' = getNewlist artistlist imageArtistList;
+    copyrightlist' = getNewlist copyrightlist imageCopyrightList;
+  in
+    if index < imgListLen
+    then
+      genList {
+        charlist = charlist // charlist';
+        artistlist = artistlist // artistlist';
+        copyrightlist = copyrightlist // copyrightlist';
+        index = index + 1;
+      }
+    else {inherit charlist artistlist copyrightlist;};
+
+  finalList = genList {};
+  farmMap = builtins.mapAttrs (key: value: {
+    name = key;
+    path = pkgs.linkFarm key value;
+  });
+  characterFolders = builtins.attrValues (farmMap finalList.charlist);
+  artistFolders = builtins.attrValues (farmMap finalList.artistlist);
+  copyrightFolders = builtins.attrValues (farmMap finalList.copyrightlist);
+
+  danbooru = {
+    characters = characterFolders;
+    artists = artistFolders;
+    copyrights = copyrightFolders;
+  };
 in
-  pkgs.linkFarm "Danbooru" (builtins.map (category: rec {
-    name = "${category}s";
-    path = pkgs.linkFarm name (getCategoryFolders category);
-  }) ["artist" "character" "copyright"])
+  pkgs.linkFarm "Danbooru" (builtins.attrValues (farmMap danbooru))
