50bc442d0a27bceae7a5810aa1de5409994935b8
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-06-30 17:00:11 +0400

    tested nixosModule

--- nix/vmConfiguration.nix
+++ nix/vmConfiguration.nix
@@ -36,9 +36,15 @@
     enable = true;
     prefetcher.enable = true;
     imgList = import ./imgList.nix;
-    filters.artists = {
-      list = ["elodeas" "yoneyama_mai" "void_0" "morncolour"];
-      invert = true;
+    filters = {
+      artists = {
+        list = ["elodeas" "yoneyama_mai" "void_0" "morncolour"];
+        invert = true;
+      };
+      previews.ratings = {
+        list = ["e" "q"];
+        invert = false;
+      };
     };
   };
 
